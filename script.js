118d1e2d07a688778a62242c862458f0de4dc723
IOrozakunov23 <islamburunduk045>
2022-03-02 15:20:59 +0300

    added 3 files

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 document.querySelectorAll(".accordion").forEach((accordion) => {
+  const parent = accordion.closest(".accordions");
+
   accordion
-      .querySelector(".accordion__header")
-      .addEventListener("click", function() {
-          accordion.classList.toggle("accordion__open");
-      });
+    .querySelector(".accordion__header")
+    .addEventListener("click", function () {
+      if (parent && parent.classList.contains("accordion__toggle")) {
+        parent.querySelectorAll(".accordion").forEach((accordion) => {
+          accordion.classList.remove("accordion__open");
+        });
+      }
+
+      accordion.classList.toggle("accordion__open");
+    });
 });

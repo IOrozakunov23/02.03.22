28588f4d7329ede0687cf6632900abdbbf1eb063
IOrozakunov23 <islamburunduk045>
2022-03-03 23:04:10 +0300

    added 3 files

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-document.querySelectorAll(".accordion").forEach((accordion) => {
-  const parent = accordion.closest(".accordions");
+// document.querySelectorAll(".accordion").forEach((accordion) => {
+//   const parent = accordion.closest(".accordions");
 
-  accordion
-    .querySelector(".accordion__header")
-    .addEventListener("click", function () {
-      if (parent && parent.classList.contains("accordion__toggle")) {
-        parent.querySelectorAll(".accordion").forEach((accordion) => {
-          accordion.classList.remove("accordion__open");
-        });
-      }
+//   accordion
+//     .querySelector(".accordion__header")
+//     .addEventListener("click", function () {
+//       if (parent && parent.classList.contains("accordion__toggle")) {
+//         parent.querySelectorAll(".accordion").forEach((accordion) => {
+//           accordion.classList.remove("accordion__open");
+//         });
+//       }
 
-      accordion.classList.toggle("accordion__open");
-    });
-});
+//       accordion.classList.toggle("accordion__open");
+//     });
+// });
+
+var firstName;
+ 
+var lastName = "Volkov";
+
+firstName = "Vasya"
+ console.log(firstName);
